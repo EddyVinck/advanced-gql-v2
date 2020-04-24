961cd039ce0e836b5597bce6c35c39d57ee3f2b4
EddyVinck <eddyvinck95@gmail.com>
2020-04-24 16:58:42 +0200

    directives stuff

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const {ApolloServer, PubSub} = require("apollo-server");
+const {ApolloServer, PubSub, SchemaDirectiveVisitor} = require("apollo-server");
 const gql = require("graphql-tag");
+const {defaultFieldResolver, GraphQLString} = require("graphql");
 
 const mockUser = {
   id: 1,
@@ -10,7 +11,43 @@ const mockUser = {
 const pubSub = new PubSub();
 const NEW_ITEM_EVENT = "NEW_ITEM_EVENT";
 
+class LogDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const oldResolver = field.resolve || defaultFieldResolver;
+
+    // allow the query (client) to add arguments
+    field.args.push({
+      type: GraphQLString,
+      name: "message",
+    });
+
+    field.resolve = (root, {message, ...rest}, ctx, info) => {
+      // schemaMessage can fall back to schema default
+      // `message` can come from the client as well!
+      const {message: schemaMessage} = this.args;
+
+      console.log("hello!", message || schemaMessage);
+
+      return oldResolver.call(
+        this, // pass the same instance of the currently visited field / type /thing
+        root, // the root of the query
+        rest, // the other arguments
+        ctx,
+        info
+      );
+    };
+  }
+  visitScalar(scalar) {
+    console.log(scalar);
+    return scalar;
+  }
+}
+
 const typeDefs = gql`
+  directive @log(
+    message: String = "my default message"
+  ) on FIELD_DEFINITION | FIELD | SCALAR
+
   type Query {
     me: User!
     settings(user: ID!): Settings!
@@ -24,9 +61,10 @@ const typeDefs = gql`
   }
 
   type User {
-    id: ID!
+    id: ID! @log(message: "id here")
     username: String!
     createdAt: Int!
+    example: String @deprecated(reason: "use this other field we made instead")
   }
 
   enum Theme {
@@ -89,6 +127,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  schemaDirectives: {
+    log: LogDirective,
+  },
   // gets created on every request
   context({connection}) {
     if (connection) {
@@ -100,5 +141,13 @@ const server = new ApolloServer({
     onConnect(params) {},
   },
 });
+
+const directiveExample = gql`
+  query getMe($yes: Boolean!) {
+    me {
+      createdAt @include(if: $yes) #skip is the inverse of include
+    }
+  }
+`;
 
 server.listen().then(({url}) => console.log(`server on ${url}`));
